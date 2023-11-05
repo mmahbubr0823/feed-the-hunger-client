6e94bc8a051fab11e915306c53a3a88634a02e44
mmahbubr0823 <mahbubfdtc@gmail.comm>
2023-11-05 21:19:14 +0600

    banner added

---
+++ src/Components/Pages/Home/Banner.jsx
@@ -0,0 +1,26 @@
+
+const Banner = () => {
+    return (
+        <div className=" flex items-center gap-3 bg-[#f2f7e8] p-4 rounded-md">
+            <div className="w-1/2">
+                <div>
+                    <h1 className="text-4xl font-semibold">Lets <span className="text-[#e95e8c]">Feed</span> Them </h1>
+                    <p className="text-sm mt-3">Feeding the hunger is a noble initiative aimed at alleviating food insecurity in our community. By collaborating with local food banks, shelters, and volunteers, this program strives to provide nourishing meals to those in need. Through food drives, fundraisers, and distribution events, it ensures that individuals and families struggling with hunger have access to essential sustenance.... </p>
+                </div>
+                <div className="flex gap-10 mt-5">
+                    <button className="btn bg-[#fc6969]">
+                        Read More
+                    </button>
+                    <button className="btn bg-[#f74cd4]">
+                        Donate
+                    </button>
+                </div>
+            </div>
+            <div className="w-1/2">
+                <img className="rounded-md" src="https://i.ibb.co/y6f805w/close-up-people-holding-apples-bag.jpg" alt="" />
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
--- src/Components/Routes/Routes.jsx
+++ src/Components/Routes/Routes.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import MainLayouts from "../LayOuts/MainLayouts/MainLayouts";
 import Login from "../Pages/Login/Login";
 import Registration from "../Pages/Registration/Registration";
+import Home from "../Pages/Home/Home";
 
 
 const myCreatedRoutes = createBrowserRouter([
@@ -9,6 +10,10 @@ const myCreatedRoutes = createBrowserRouter([
       path: "/",
       element: <MainLayouts></MainLayouts>,
       children:[
+        {
+            path:'/',
+            element:<Home></Home>
+        },
         {
             path:'/login',
             element:<Login></Login>
